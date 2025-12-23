e5f3992ae2a1e431ecc5a92247a33d659f06754f
Gaurav Sharma <igaurav1002@outlook.com>
2025-12-23 04:40:17 +0000

    initial terraform menifest for VNET module

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "vnet_name" {
+  description = "The name of the virtual network."
+  type        = string
+}
+
+variable "address_space" {
+  description = "The address space for the virtual network."
+  type        = list(string)
+}
+
+variable "location" {
+  description = "The Azure region where the virtual network will be created."
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group."
+  type        = string
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to the resource."
+  type        = map(string)
+  default     = {}
+}
